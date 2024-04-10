6d9e9749523cb01b7e5e7865833bcc51260ade53
vishalseedor <vishalviswam465@gmail.com>
2024-04-10 12:20:17 +0530

    correction

--- lib/Helpers/Images/images.dart
+++
@@ -1,8 +0,0 @@
-const path = 'assets/';
-const loginBackgroundImages = 'assets/logoimage.jpg';
-const splashImage = 'assets/spalsh.png';
-const registerBackgroundImages='assets/K.png';
-const categoryDogImage='assets/dog.png';
-const categoryCatImage='assets/cat.png';
-const categoryHorseImage='assets/horse.png';
-const categoryRabbitImage='assets/rabbit.png';
